20cc7ec96dee905d56ddae32cc1a84e14012c343
charles1965 <karl.wajand@gmx.net>
2018-11-25 16:11:55 +0100

    ddd

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 		System.out.println("Hello world");
 		System.out.println("I'm adding code to my project");
 		System.out.println("Adding code from GitHub");
+		System.out.println("changed in github directly);
 	}
 }
